e1fb7921cbf0d3336623b099843f1b9fda44ddeb
Nerdomancer <kerosamyriad@gmail.com>
2026-05-02 11:39:00 +0300

    file upload

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
 import express from 'express';
 
+const fileUpload = require("express-fileupload");
+
 const PORT = 3012;
 const app = express();
 
+app.use(fileUpload());
+
+app.post("/upload", function (req: Request, res: Response){
+	if(req.files && Object.keys(req.files).length !== 0){
+		const uploadedFile = req.files.uploadFile;
+		console.log(uploadedFile);
+		const uploadPath = __dirname + "/uploads/" + uploadedFile.name;
+		console.log(uploadPath);
+		uploadedFile.mv(uploadPath, function (err) {
+		if (err) {
+			console.log(err);
+			res.send("Failed !!");
+		} else res.send("Successfully Uploaded !!");
+    });
+	}
+})
+
 app.get('/', (req: Request, res: Response) => {
 	console.log("testing testing");
-	res.send("wassup");
+	res.sendFile("/home/nerdomancerkirou/projects/printserver/html/index.html");
 });
 
 app.listen(PORT, ()=>{
